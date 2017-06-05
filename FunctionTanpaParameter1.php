2ad72b406b0de0410002413642feb7549184ab00
dionrahdiono <dionrhd@gmail.com>
2017-06-05 09:36:38 +0700

    Add files via upload

---
+++ FunctionTanpaParameter1.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+<title>Belajar Function dalam PHP</title>
+</head>
+<body>
+<?php
+function panggil()
+{
+	echo"Merupakan function tanpa parameter <br>";
+	echo"Wah... Mudah yah..? ";
+}
+
+panggil();
+
+?>
+</body>
+</html>
---
+++ FunctionTanpaParameter2.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+<title>Belajar Function dalam PHP</title>
+</head>
+<body>
+<?php
+function panggil()
+{
+	echo"Merupakan function tanpa parameter <br>";
+	echo"Wah... Mudah yah..? ";
+}
+	echo"<u>Dipanggil pertama kali </u><br>";
+panggil();
+	echo"<br><br><u>Dipanggil untuk kedua kalinya</u><br>";
+panggil();
+?>
+</body>
+</html>
